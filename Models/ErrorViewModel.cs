19069490185882a9b5964c3eec0a3755bc1c9054
karmegams02 <karmegam.seerangan@syncfusion.com>
2025-08-08 15:03:52 +0530

    Added the HTMl to PDF conversion project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HTMLtoPDF_GCP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
